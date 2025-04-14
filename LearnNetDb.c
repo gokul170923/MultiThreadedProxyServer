14a6bd55e258727da363622ba5f9c70325d5ad0c
Gokul Singh <jaie6252@gmail.com>
2025-04-14 14:44:01 +0530

    closing

--- LearnNetDb.c
+++ LearnNetDb.c
@@ -27,13 +27,70 @@
 */
 
 #include <stdio.h>      // printf() 
-#include <stdlib.h>     // malloc() 
+#include <stdlib.h>     // malloc() , exit(1)
 #include <string.h>     // memset()
 #include <netdb.h>      // getaddinfo() 
-#include <arpa/inet.h>  // for ip related tasks , read LearnInet
+#include <arpa/inet.h>  // for ip related tasks , convert the ip to different format
 
-int main(){
+int main(int argc , char * argv[]){
+
+        if(argc==1){
+                fprintf(stderr,"hostname not entered\n");
+                return 1;
+        }
         
+        struct addrinfo req , *res;    
+        // req will tell the library what we need and the response is lined list of type addrinfo
+
+        int status;     // getinfoaddr() also returns a status
+
+        memset(&req,0,sizeof(req));     // remove garbage from memory
+
+        req.ai_family = AF_INET;
+        req.ai_socktype = SOCK_STREAM;
+        /*
+                the addrinfo struct has these members ( more)
+                struct addrinfo {
+                    int ai_flags;
+                    int ai_family;      // address info family (like AF_INET for ipv4 or AF_INET6 for ipv6)
+                    int ai_socktype;    // socket type (like SOCK_STREAM for tcp or SOCK_DGRAM for udp)
+                    int ai_protocol;    // protocol (like IPPROTO_TCP)
+                    struct sockaddr *ai_addr;      // Pointer to struct sockaddr (the actual address)
+                    char            *ai_canonname; // Canonical hostname (optional)
+                    struct addrinfo *ai_next;      // Pointer to next result (linked list)
+                };
+
+        */
+
+        status = getaddrinfo(argv[1],NULL,&req,&res);
+        // NULL param : you can request port as well , on success we get 0 else non zero
+
+        if(status!=0){
+                fprintf(stderr,"getaddrinfo gave an error: %s\n",gai_strerror(status));
+                // gai_strerro converts the error given by getaddrinfo to a readable string
+                return 1;
+        }
+
+        for(struct addrinfo * itr = res;itr!=NULL;itr = itr->ai_next){
+                // since response is a linked list i can iterate over it
+
+                char ipAsString[INET_ADDRSTRLEN];
+                // this is just to store ip as a string
+                // inet_addrstrlen is a Macro in inet which tells ipv4 address length as a string
+
+                struct sockaddr_in *sockaddr_ptr = (struct sockaddr_in *) itr->ai_addr;
+                // in structure od addrinfo ai_addr was a adress of type struct sockaddr 
+                // tyoe sockaddrrepresents all kinds of sicket addresss in networking 
+                // we just type casted it to type sockaddr_in for ipv4 adress only
+
+                inet_ntop(AF_INET,&sockaddr_ptr->sin_addr,ipAsString,sizeof(ipAsString));
+                // convert the ip (currently as bit) to string 
+                //params : AF_INET(type ipv4) , source , destination , size of destinatin
+
+                printf("Resolved IP Address: %s , Canonical name : %s\n",ipAsString,itr->ai_canonname);
+
+        }
+
         return 0;
 }
 
