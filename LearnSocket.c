153fe801d402f9147f7fff4b0ffb8c58b8c21ef1
Gokul Singh <jaie6252@gmail.com>
2025-04-15 20:23:17 +0530

    start learning sockets

---
+++ LearnSocket.c
@@ -0,0 +1,6 @@
+/*
+        Main new library : <sys/socket.h>     
+        This header provides core socket-related functions, structures, and constants
+        which are then  used in both client and server code ( it is a system level header sys)                 
+
+*/
